7bccecd23baff0ca4e4e79eeac2c30e35fce2a21
Duy Nguyen <milonguyen95@outlook.com>
2021-09-03 11:15:26 +0000

    Update mongo config

--- src/configs/mongo.config.js
+++ src/configs/mongo.config.js
@@ -3,39 +3,39 @@ const { get } = require('./env.config');
 
 mongoose.Promise = global.Promise;
 const mongoConfig = {
-	useNewUrlParser: true,
-	useUnifiedTopology: true,
-	ignoreUndefined: true,
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        ignoreUndefined: true,
 };
 
 const uri = get('mongo_uri');
 async function connectDB() {
-	try {
-		await mongoose.connect(uri, mongoConfig);
-		return console.log(`Connected DB !`);
-	} catch (error) {
-		throw new Error(error);
-	}
+        try {
+                await mongoose.connect(uri, mongoConfig);
+                return console.log(`Connected DB !`);
+        } catch (error) {
+                throw new Error(error);
+        }
 }
 async function disconnectDB() {
-	try {
-		await mongoose.connection.close();
-		return console.log(`Disconnected DB !`);
-	} catch (error) {
-		throw new Error(error);
-	}
+        try {
+                await mongoose.connection.close();
+                return console.log(`Disconnected DB !`);
+        } catch (error) {
+                throw new Error(error);
+        }
 }
 
 async function clearDB() {
-	try {
-		const collections = mongoose.connection.collections;
-		for (const key in collections) {
-			const collection = collections[key];
-			await collection.deleteMany();
-		}
-	} catch (error) {
-		throw new Error(error.message);
-	}
+        try {
+                const collections = mongoose.connection.collections;
+                for (const key in collections) {
+                        const collection = collections[key];
+                        await collection.deleteMany();
+                }
+        } catch (error) {
+                throw new Error(error.message);
+        }
 }
 
 module.exports = { mongoConfig, connectDB, disconnectDB, clearDB };
