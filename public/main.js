a957586e33671744e3f9bf094ab07779046debe6
Duy Nguyen <milonguyen95@outlook.com>
2021-09-05 11:37:52 +0700

    Update UI

--- public/main.js
+++ public/main.js
@@ -1,10 +1,8 @@
-appConfig = {
-    YOUTUBE_API_URL: 'https://youtube.googleapis.com/youtube/v3/search?part=snippet&key=AIzaSyAoLqPULiJ7dIltlMArI08b6VOK6ECue8k&type=video'
-}
-
 window.onload = async() => {
     updateUI();
 }
+
+var player;
 
 const updateUI = async() => {
     const fullName = document.getElementById('full-name');
@@ -26,36 +24,67 @@ const updateUI = async() => {
 };
 
 const socket = io();
-socket.on("connect", () => {
-    // console.log(socket.id);
-})
-
-socket.on("playingVideo", async (data) => {
-    // console.log(data);
+socket.on("playingVideo", async(data) => {
     if (data.playingVideo) {
-        let elementPlayingVideo = `<iframe id="video-iframe" src="https://www.youtube.com/embed/${data.playingVideo.youtubeVideoId}?autoplay=1&start=${data.playedTime}" allow="autoplay" allowfullscreen>`
-        // let elementPlayingVideo = `<iframe id="video-iframe" src="https://youtu.be/${data.playingVideo.youtubeVideoId}?t=${data.playedTime}" frameborder="0" allowfullscreen>`
-        document.getElementById('videoPlaying').innerHTML = elementPlayingVideo
+        player = new YT.Player('videoPlaying', {
+            height: '390',
+            width: '640',
+            videoId: `${data.playingVideo.youtubeVideoId}`,
+            enablejsapi: 1,
+            playerVars: {
+                'autoplay': 1,
+                'controls': 0,
+                'mute': 1,
+                'start': `${data.playedTime}`,
+            },
+            events: {
+                'onReady': onPlayerReady,
+            }
+        });
         document.getElementById('titlePlayingVideo').innerHTML = `<h4>${data.playingVideo.title}</h4>`
         updateCount(data.playingVideo._id)
+        init(data.playingVideo);
     }
 })
 
+function onPlayerReady(event) {
+    event.target.setVolume(100);
+    event.target.playVideo();
+
+}
+
+function changeMute() {
+    if (player.isMuted()) player.unMute();
+    else {
+        player.mute();
+    }
+}
+// var done = false;
+
+// function onPlayerStateChange(event) {
+//     if (event.data == YT.PlayerState.PLAYING && !done) {
+//         setTimeout(stopVideo, 6000);
+//         done = true;
+//     }
+// }
+
+// function stopVideo() {
+//     player.stopVideo();
+// }
+
 function updateCount(id) {
-    countLikeVideo(id).then(async (response) => {
+    countLikeVideo(id).then(async(response) => {
         const user = await axios.get(`/user/me`);
         const { email } = user.data;
         let elementLike = ""
         let elementDislike = ""
         if (response.likes.findIndex(x => x.authorEmail == email) >= 0) {
             elementLike = `<h5><i class="like fas fa-thumbs-up" onclick="unlike('${id}')"></i> <span id="likeCount">${response.likes.length}</span> Like</h5>`
-        }
-        else elementLike = `<h5><i class="like fas fa-thumbs-up" onclick="like('${id}')"></i> <span id="likeCount">${response.likes.length}</span> Like</h5>`
-    
+        } else elementLike = `<h5><i class="like fas fa-thumbs-up" onclick="like('${id}')"></i> <span id="likeCount">${response.likes.length}</span> Like</h5>`
+
         if (response.dislikes.findIndex(x => x.authorEmail == email) >= 0) {
             elementDislike = `<h5><i class="dislike fas fa-thumbs-down" onclick="unDislike('${id}')"></i> <span id="dislikeCount">${response.dislikes.length}</span> Dislike</h5>`
-        }
-        else elementDislike = `<h5><i class="dislike fas fa-thumbs-down" onclick="dislike('${id}')"></i> <span id="dislikeCount">${response.dislikes.length}</span> Dislike</h5>`
+        } else elementDislike = `<h5><i class="dislike fas fa-thumbs-down" onclick="dislike('${id}')"></i> <span id="dislikeCount">${response.dislikes.length}</span> Dislike</h5>`
 
         let elementReactLike = `<div class="d-inline-flex justify-content-between">
             ${elementLike}
--- public/video.js
+++ public/video.js
@@ -4,99 +4,97 @@ let videoList = [];
 let videoPlaying = {};
 
 async function getAll() {
-  let result = await axios({
-    url: ROUTE + "/",
-    method: "GET",
-  });
-  return result.data;
+    let result = await axios({
+        url: ROUTE + "/",
+        method: "GET",
+    });
+    return result.data;
 }
 
 async function getById(id) {
-  let result = await axios({
-    url: ROUTE + "/" + id,
-    method: "GET",
-  });
-  return result.data;
+    let result = await axios({
+        url: ROUTE + "/" + id,
+        method: "GET",
+    });
+    return result.data;
 }
 
 async function likeVideo(id) {
-  let result = await axios({
-    url: ROUTE + "/like/" + id,
-    method: "POST",
-  });
-  return result.data;
+    let result = await axios({
+        url: ROUTE + "/like/" + id,
+        method: "POST",
+    });
+    return result.data;
 }
 
 async function unlikeVideo(id) {
-  let result = await axios({
-    url: ROUTE + "/like/" + id,
-    method: "DELETE",
-  });
-  return result.data;
+    let result = await axios({
+        url: ROUTE + "/like/" + id,
+        method: "DELETE",
+    });
+    return result.data;
 }
 
 async function dislikeVideo(id) {
-  let result = await axios({
-    url: ROUTE + "/dislike/" + id,
-    method: "POST",
-  });
-  return result.data;
+    let result = await axios({
+        url: ROUTE + "/dislike/" + id,
+        method: "POST",
+    });
+    return result.data;
 }
 
 async function unDislikeVideo(id) {
-  let result = await axios({
-    url: ROUTE + "/dislike/" + id,
-    method: "DELETE",
-  });
-  return result.data;
+    let result = await axios({
+        url: ROUTE + "/dislike/" + id,
+        method: "DELETE",
+    });
+    return result.data;
 }
 
 async function countLikeVideo(id) {
-  let result = await axios({
-    url: ROUTE + "/count/" + id,
-    method: "GET",
-  });
-  return result.data;
+    let result = await axios({
+        url: ROUTE + "/count/" + id,
+        method: "GET",
+    });
+    return result.data;
 }
 
 async function like(id) {
-  await likeVideo(id);
-  updateCount(id);
+    await likeVideo(id);
+    updateCount(id);
 }
 
 async function unlike(id) {
-  await unlikeVideo(id);
-  updateCount(id);
+    await unlikeVideo(id);
+    updateCount(id);
 }
 
 async function dislike(id) {
-  await dislikeVideo(id);
-  updateCount(id);
+    await dislikeVideo(id);
+    updateCount(id);
 }
 
 async function unDislike(id) {
-  await unDislikeVideo(id);
-  updateCount(id);
+    await unDislikeVideo(id);
+    updateCount(id);
 }
 
-async function init() {
-  await getAll().then((resposne) => {
-    // console.log(resposne);
-    if (resposne.length == 0) return console.log("Empty");
-    let element = "";
-    videoList = resposne;
-    videoList.map((res) => {
-      element += `<div id="item-video">
-          <img class="rounded" src="${res.thumbnailUrl}" width="200" height="100%" />
-          <div id="video-detail">
-              <h5 class="title" style="color: #ffb347">${res.title}</h5>
+async function init(playingVideo) {
+    await getAll().then((resposne) => {
+        // console.log(resposne);
+        if (resposne.length == 0) return console.log("Empty");
+        let element = "";
+        videoList = resposne;
+        videoList.filter((video) => video.youtubeVideoId !== playingVideo.youtubeVideoId).map((res) => {
+            element += `<div class="item-video">
+          <img class="rounded" src="${res.thumbnailUrl}" width="200" height="100%" style="min-width: 200px"/>
+          <div class="video-detail">
+              <h5 class="title" style="color: #ffb347; font-size: 1rem">${res.title}</h5>
               <small class="author">Add by: ${res.authorEmail}</small>
               <p>${res.views} lượt xem</p>
           </div>
       </div>`;
+        });
+        document.getElementById("list-video").innerHTML = element;
     });
-    document.getElementById("list-video").innerHTML = element;
-  });
 }
-
-init();
