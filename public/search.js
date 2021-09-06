f2b92df6103a5c365dc3f0a0676c2a7df1f14d3b
Duy Nguyen <milonguyen95@outlook.com>
2021-09-06 10:05:45 +0700

    Fix search page

--- public/search.js
+++ public/search.js
@@ -6,6 +6,34 @@ searchValue.oninput = (e) => {
     searchValue.value = e.target.value;
 };
 
+window.onload = async() => {
+    updateUI();
+}
+
+const updateUI = async() => {
+    const fullName = document.getElementById('full-name');
+    const avatar = document.getElementById('avatar');
+
+    try {
+        const response = await axios.get(`/user/me`);
+        const { nickname, picture } = response.data;
+
+        const isAuthenticated = nickname && picture;
+        document.getElementById("btn-login").style.visibility = isAuthenticated ? 'hidden' : 'visible';
+        document.getElementsByClassName("user-profile")[0].style.display = isAuthenticated ? 'block' : 'none';
+
+        if (nickname && picture) {
+            fullName.innerHTML = nickname;
+            avatar.src = picture;
+            fullName.style.visibility = 'visible';
+            avatar.style.visibility = 'visible';
+        }
+    } catch (error) {
+        document.getElementsByClassName("user-profile")[0].style.display = 'none';
+        console.log(error);
+    }
+};
+
 const searchVideo = async(value) => {
     try {
         const response = await axios.get(`/video/search?keyword=${value}`)
